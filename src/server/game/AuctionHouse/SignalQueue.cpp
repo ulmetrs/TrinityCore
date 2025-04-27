b16e31b264895cdde1d849f859be41cb5ddf3d5f
ulmetrs <robertulmet@gmail.com>
2025-04-27 12:30:37 -0400

    Update SignalQueue.cpp

--- src/server/game/AuctionHouse/SignalQueue.cpp
+++ src/server/game/AuctionHouse/SignalQueue.cpp
@@ -15,8 +15,9 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "SignalQueue.h"
-#include "AuctionHouseCommon.h"
+ #include "SignalQueue.h"
+ #include "AuctionHouseCommon.h"
+ #include "AuctionHouseMgr.h" // Ensure all auction-related types are available
 
 template<typename T>
 void SignalQueue<T>::send(T value, std::stop_token stop) {
