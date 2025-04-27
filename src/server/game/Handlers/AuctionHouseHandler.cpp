4a54e2c9b9218bfa07af905ae2f85b0dbd0d326f
ulmetrs <robertulmet@gmail.com>
2025-04-27 08:46:43 -0400

    Update AuctionHouseHandler.cpp

--- src/server/game/Handlers/AuctionHouseHandler.cpp
+++ src/server/game/Handlers/AuctionHouseHandler.cpp
@@ -698,7 +698,7 @@ void WorldSession::HandleAuctionListBidderItems(WorldPacket& recvData)
     if (!ahEntry)
         return;
 
-    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->houseId);
+    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->ID);
 
     // Client sends this list, which I'm honestly not entirely sure why?
     std::vector<uint32> auctionIds;
@@ -741,7 +741,7 @@ void WorldSession::HandleAuctionListOwnerItems(WorldPacket& recvData)
     if (!ahEntry)
         return;
 
-    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->houseId);
+    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->ID);
 
     auto request = std::make_unique<AuctionSearchOwnerListRequest>(auctionHouseFaction, GetPlayer()->GetGUID());
     sAuctionMgr->GetAuctionHouseSearcher()->QueueSearchRequest(std::move(request));
@@ -812,7 +812,7 @@ void WorldSession::HandleAuctionListItems(WorldPacket& recvData)
     if (!ahEntry)
         return;
 
-    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->houseId);
+    uint8 auctionHouseFaction = AuctionHouseMgr::GetAuctionHouseFactionFromHouseId(ahEntry->ID);
 
     AuctionHouseSearchInfo ahSearchInfo;
     ahSearchInfo.wsearchedname = wsearchedname;
