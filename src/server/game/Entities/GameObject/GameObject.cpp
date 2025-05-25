4528a5114e49cad9a7dcf971f7aac5ab1d3484e6
ulmetrs <robertulmet@gmail.com>
2025-05-25 14:50:08 -0400

    add swap test

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1129,6 +1129,13 @@ bool GameObject::LoadFromDB(ObjectGuid::LowType spawnId, Map* map, bool addToMap
         return false;
     }
 
+    // Test swapping copper ore to silver, and see if it effects all respawns
+    if (data->id == 1731)
+    {
+        data->id = 1733;
+        TC_LOG_DEBUG("nodeswap", "Swapping copper ore to silver for spawnId: {}", spawnId);
+    }
+
     uint32 entry = data->id;
     //uint32 map_id = data->mapid;                          // already used before call
     uint32 phaseMask = data->phaseMask;
