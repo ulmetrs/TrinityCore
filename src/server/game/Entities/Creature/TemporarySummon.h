11b04f400951b7e3b104cd8a3699caa5d0b66c0a
ulmetrs <robertulmet@gmail.com>
2025-05-26 09:47:55 -0400

    Update TemporarySummon.h

--- src/server/game/Entities/Creature/TemporarySummon.h
+++ src/server/game/Entities/Creature/TemporarySummon.h
@@ -38,7 +38,7 @@ class TC_GAME_API TempSummon : public Creature
         explicit TempSummon(SummonPropertiesEntry const* properties, WorldObject* owner, bool isWorldObject);
         virtual ~TempSummon() { }
         void Update(uint32 time) override;
-        virtual void InitStats(uint32 lifetime, uint8 levelOverride /*= 0*/);
+        virtual void InitStats(uint32 lifetime, uint8 levelOverride = 0);
         virtual void InitSummon();
         void UpdateObjectVisibilityOnCreate() override;
         virtual void UnSummon(uint32 msTime = 0);
@@ -76,7 +76,7 @@ class TC_GAME_API Minion : public TempSummon
 {
     public:
         Minion(SummonPropertiesEntry const* properties, Unit* owner, bool isWorldObject);
-        void InitStats(uint32 duration, uint8 levelOverride /*= 0*/) override;
+        void InitStats(uint32 duration, uint8 levelOverride = 0) override;
         void RemoveFromWorld() override;
         void setDeathState(DeathState s) override;
         Unit* GetOwner() const { return m_owner; }
@@ -102,7 +102,7 @@ class TC_GAME_API Guardian : public Minion
 {
     public:
         Guardian(SummonPropertiesEntry const* properties, Unit* owner, bool isWorldObject);
-        void InitStats(uint32 duration, uint8 levelOverride /*= 0*/) override;
+        void InitStats(uint32 duration, uint8 levelOverride = 0) override;
         bool InitStatsForLevel(uint8 level);
         void InitSummon() override;
 
@@ -128,7 +128,7 @@ class TC_GAME_API Puppet : public Minion
 {
     public:
         Puppet(SummonPropertiesEntry const* properties, Unit* owner);
-        void InitStats(uint32 duration, uint8 levelOverride /*= 0*/) override;
+        void InitStats(uint32 duration, uint8 levelOverride = 0) override;
         void InitSummon() override;
         void Update(uint32 time) override;
 };
