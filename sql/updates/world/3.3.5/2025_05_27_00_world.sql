5db9e0bf58e02ee03880810721e9bb64a706cfbc
ulmetrs <robertulmet@gmail.com>
2025-05-27 18:46:47 -0400

    Update 2025_05_27_00_world.sql

--- sql/updates/world/3.3.5/2025_05_27_00_world.sql
+++ sql/updates/world/3.3.5/2025_05_27_00_world.sql
@@ -1,2 +1,5 @@
+DELETE FROM `command`
+WHERE `name` IN ('onlogin');
+
 INSERT INTO `command` (`name`, `help`) VALUES
 ('onlogin', 'Syntax: .onlogin [$playername] [$targetcommand]\r\n\r\nStores a command to be executed when the player logs in.');
