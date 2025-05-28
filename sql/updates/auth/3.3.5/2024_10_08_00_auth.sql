74a17b9e0ba27ab22a7736f4efd027bfe47f3745
ulmetrs <robertulmet@gmail.com>
2025-05-27 20:33:00 -0400

    fixes

--- sql/updates/auth/3.3.5/2024_10_08_00_auth.sql
+++ sql/updates/auth/3.3.5/2024_10_08_00_auth.sql
@@ -1,2 +1,2 @@
-replace into `rbac_permissions` (`id`, `name`) values('1011','Command: onlogin');
-replace into `rbac_linked_permissions` (`id`, `linkedId`) values('196','1011');
+replace into `rbac_permissions` (`id`, `name`) values('1009','Command: tele instanceid');
+replace into `rbac_linked_permissions` (`id`, `linkedId`) values('198','1009')
