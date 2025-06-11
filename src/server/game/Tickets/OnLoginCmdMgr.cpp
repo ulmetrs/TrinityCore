9c9c30f37bcc3d2afc924cd06e7840448dce7507
ulmetrs <robertulmet@gmail.com>
2025-06-11 00:08:44 -0400

    add debugs

--- src/server/game/Tickets/OnLoginCmdMgr.cpp
+++ src/server/game/Tickets/OnLoginCmdMgr.cpp
@@ -138,6 +138,7 @@ void OnLoginCmdMgr::AddCommand(ObjectGuid playerGuid, std::string const& command
 
 const std::vector<OnLoginCmd*>& OnLoginCmdMgr::GetCommandsForPlayer(ObjectGuid const& guid) const
 {
+    TC_LOG_DEBUG("onlogin", "OnLoginCmdMgr::GetCommandsForPlayer {}", guid);
     static const std::vector<OnLoginCmd*> empty;
     auto itr = _onLoginCommandList.find(guid);
     return itr != _onLoginCommandList.end() ? itr->second : empty;
@@ -145,6 +146,7 @@ const std::vector<OnLoginCmd*>& OnLoginCmdMgr::GetCommandsForPlayer(ObjectGuid c
 
 void OnLoginCmdMgr::ClearCommandsForPlayer(ObjectGuid const& guid)
 {
+    TC_LOG_DEBUG("onlogin", "OnLoginCmdMgr::ClearCommandsForPlayer {}", guid);
     auto itr = _onLoginCommandList.find(guid);
     if (itr == _onLoginCommandList.end())
         return;
