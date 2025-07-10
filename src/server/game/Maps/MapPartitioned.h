3e96839e8610817048bafcae5b86450236795adf
ulmetrs <robertulmet@gmail.com>
2025-07-10 00:18:18 -0400

    Update MapPartitioned.h

--- src/server/game/Maps/MapPartitioned.h
+++ src/server/game/Maps/MapPartitioned.h
@@ -54,9 +54,9 @@ class TC_GAME_API MapPartitioned : public Map
 
         Partitions &GetPartitions() { return _partitions; }
         PartitionEntries &GetPartitionEntries() { return _partitionEntries; }
-        MapPartition* GetPartitionEntry(uint32 partitionId)
+        const MapPartition* GetPartitionEntry(uint32 partitionId) const;
         {
-            for (auto& entry : _partitionEntries)
+            for (const auto& entry : _partitionEntries)
             {
                 if (entry.partitionId == partitionId)
                     return &entry;
