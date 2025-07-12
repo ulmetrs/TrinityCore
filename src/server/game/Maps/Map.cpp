7e0cc957a18498f10605412c230cd6b7e04769d1
ulmetrs <robertulmet@gmail.com>
2025-07-11 21:43:35 -0400

    Update Map.cpp

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -998,6 +998,22 @@ void Map::Update(uint32 t_diff)
             if (!player || !player->IsInWorld())
                 continue;
 
+            if (GetId() == 0)
+            {
+                m_creatureQuadTreeLogTimer += t_diff;
+                if (m_creatureQuadTreeLogTimer >= 1000)
+                {
+                    m_creatureQuadTreeLogTimer = 0;
+
+                    Trinity::ObjectCounter gridCounter;
+                    Cell::VisitAllObjects(player, gridCounter, MAP_SIZE);
+                    TC_LOG_DEBUG("quadtrees", "Finish Grid Count on map {} - found {}", GetId(), gridCounter.count);
+                    Trinity::ObjectCounter quadCounter;
+                    _quadTree->QueryAll(MAPQT_ALL, quadCounter);
+                    TC_LOG_DEBUG("quadtrees", "Finish Quad Count on map {} - found {}", GetId(), quadCounter.count);
+                }
+            }
+
             // update players at tick
             player->Update(t_diff);
 
